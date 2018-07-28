c404dc9912f4c78821a4a48f7c477e0c5fa30ff5
Boullon Daniel <daniel.boullon@yahoo.com>
2018-07-28 12:44:57 -0300

    falta de zaranga te tiene loca

--- app/src/main/java/com/grupo214/usuario/activities/MainActivity.java
+++ app/src/main/java/com/grupo214/usuario/activities/MainActivity.java
@@ -124,6 +124,7 @@ public class MainActivity extends AppCompatActivity
 
         //barra superior
         setSupportActionBar(toolbar);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
